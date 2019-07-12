5c014bae82d06d2a7f23cffb61237751ef955056
FKYnJYQ <loveress01@gmail.com>
2019-07-12 15:56:54 +0800

    feat: init, finish outlook

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,170 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <!-- <v-flex fluid fill-height> -->
+  <div>
+    <v-layout row>
+      <v-flex xs8 id="grey-color" style="height:100vh;">
+        <v-layout column justify-space-between fill-height>
+          <v-card flat color="#f5f6f8">
+            <v-container>
+              <v-layout align-center>
+                <v-flex>
+                  <v-icon class="icon-large">school</v-icon>
+                  <span class="font-weight-bold large"
+                    >&nbsp;&nbsp;&nbsp;教务系统统一身份认证</span
+                  >
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+
+          <v-container fill-height>
+            <v-layout fill-height align-center>
+              <v-flex fill-height>
+                <div id="my-pic"></div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+
+          <v-card flat color="#f5f6f8">
+            <v-container>
+              <v-layout align-center>
+                <v-flex>
+                  <v-flex
+                    >&copy; {{ new Date().getFullYear() }} 高校教务处</v-flex
+                  >
+                  <v-spacer></v-spacer>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-layout>
+      </v-flex>
+      <v-flex xs4 style="background:white;height:100vh;">
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex>
+              <v-card flat color="white">
+                <v-card-title class="super-large font-weight-bold"
+                  >登录/注册</v-card-title
+                >
+                <v-card-text>
+                  <template v-for="(v, index) in inputs">
+                    <v-flex :key="index + 'i'" mt-3 class="font-weight-bold">{{
+                      v.label + (v.rq ? "*" : "")
+                    }}</v-flex>
+                    <v-text-field
+                      mt-0
+                      :key="index + 'v'"
+                      :placeholder="v.ph"
+                      background-color="#f5f6f8"
+                      required
+                    ></v-text-field>
+                  </template>
+                  <v-flex mt-3 class="font-weight-bold">身份*</v-flex>
+                  <v-select
+                    :items="identities"
+                    v-model="select"
+                    required
+                    background-color="#f5f6f8"
+                  ></v-select>
+                  <v-btn
+                    depressed
+                    round
+                    block
+                    dark
+                    color="#3A9BFC"
+                    class="font-weight-bold medium"
+                    >登录</v-btn
+                  >
+                </v-card-text>
+
+                <v-card-action>{{ select }}</v-card-action>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
+function trans(params) {
+  let length = 0;
+  for (let key in params) {
+    length = params[key].length;
+    break;
+  }
+  let a = new Array(length);
+  for (let i = 0; i < a.length; i++) {
+    a[i] = new Object();
+    for (let key in params) {
+      a[i][key] = params[key][i];
+    }
+  }
+  return a;
+}
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      select: null,
+      inputs: trans({
+        label: ["用户名", "学号", "密码"],
+        ph: [
+          "Please input your name.",
+          "Please input your student ID.",
+          "Your credentials are kept private."
+        ],
+        rq: [true, true, true]
+      }),
+      identities: trans({
+        text: ["本科生", "研究生"],
+        value: [0, 1]
+      }),
+      myImage: require("@/assets/AIP_login_background.png")
+    };
+  },
+  methods: {
+    get_first_identity: function() {
+      return this.$data.identities[0];
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+/* @import url("https://use.fontawesome.com/releases/v5.1.0/css/all.css"); */
+@import url("https://fonts.googleapis.com/css?family=Noto+Serif+SC&display=swap");
+* {
+  font-family: "Noto Serif SC", serif;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.icon-large {
+  font-size: 3em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.medium {
+  font-size: 1.3em;
 }
-a {
-  color: #42b983;
+.large {
+  font-size: 2.4em;
+}
+.super-large {
+  font-size: 2.8em;
+}
+#my-pic {
+  background: url("../assets/AIP_login_background.png") no-repeat center center;
+  -webkit-background-size: contain;
+  -moz-background-size: contain;
+  -o-background-size: contain;
+  background-size: contain;
+  width: auto;
+  height: 100%;
+  /* transform: scale(0.5); */
+}
+.full-height {
+  height: 100vh;
+}
+#grey-color {
+  background-color: #f5f6f8;
 }
 </style>
